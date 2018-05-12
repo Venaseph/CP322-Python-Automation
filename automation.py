5c4f11d91afa6846b7eba90dd409717526405c14
Chris Peragine <cperagine@gmail.com>
2018-05-12 10:52:13 -0400

    Segmented rmrfOldData()

--- automation.py
+++ automation.py
@@ -2,6 +2,7 @@
 import sys
 import os
 
+
 # Global Constants
 CLONE_REPO = "git clone https://github.com/ccoble-southhills/cp332-project6.git"
 REPO_DIR = "cp332-project6"
@@ -12,13 +13,17 @@ def main():
 
 
 def gitIt():
-    if os.path.isdir(REPO_DIR): # Check if directory exists
-        os.system(RM_RF + REPO_DIR) # Recusively delete files and folders if it does
+    rmrfOldData() # Check for existing data / rm rf if exists
     try:
         os.system(CLONE_REPO) # Clone Repo
         print("Repo Cloned Successfully!")
     except Exception as ex:
         exceptionDef(ex)
+
+
+def rmrfOldData():
+     if os.path.isdir(REPO_DIR): # Check if directory exists
+        os.system(RM_RF + REPO_DIR) # Recusively delete files and folders if it does   
 
 
 def exceptionDef(ex):
