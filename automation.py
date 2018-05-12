3c7b4ef1ade501f27c977f7bca0516e8e5a2a1e1
Chris Peragine <cperagine@gmail.com>
2018-05-12 09:18:31 -0400

    RepoClones

--- automation.py
+++ automation.py
@@ -1,8 +1,27 @@
 # !/usr/bin/env python
 import sys
+import os
+
+# Global Constants
+CLONE_REPO = "git clone https://github.com/ccoble-southhills/cp332-project6.git"
 
 def main():
-    pass
+    gitIt()
+
+
+def gitIt():
+    #os.system("sshpass -p your_password ssh user_name@your_localhost")
+    #os.chdir(REPO_PATH) # Move to Clone folder
+    try:
+        os.system(CLONE_REPO) # Clone Repo
+        print("Repo Cloned Successfully!")
+    except Exception as ex:
+        exceptionDef(ex)
+
+
+def exceptionDef(ex):
+    print(ex)        
+
 
 if __name__ == "__main__":
     sys.exit(main())
