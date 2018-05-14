6ac843ebb15abd88d4bc747ea14a641f4196c31d
Chris Peragine <cperagine@gmail.com>
2018-05-13 22:02:13 -0400

    Cleaned up exkill(), globals from returns for ease of use.

--- automation.py
+++ automation.py
@@ -9,28 +9,48 @@ CLONE_REPO = "git clone https://github.com/ccoble-southhills/cp332-project6.git"
 REPO_DIR = "cp332-project6"
 RM_RF = "rm -rf "
 
+#Global Vars
+testCaseResults = {'Pass': 0, 'Fail': 0}
+version = None
+revHash = None
+
 
 def main():
     # Remove Old / Get New
     gitIt()
-    revHash = gitRevisionHash()
+    # Retrieve Repo Hash and Versions
+    gitVersion()
+    gitRevisionHash()
+
+    print(version)
     print(revHash)
 
 
-def gitRevisionHash():
-    # Move to correct DIR
+def gitVersion():
+    global version
+
     os.chdir(REPO_DIR)
-    # return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip()
-    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
+    try:
+        with open('VERSION', 'r') as versionFile:
+            # Grab version, remove newline
+            version = versionFile.read()[:-1]
+    except Exception as ex:
+        exceptionKillDef(ex)
+
+
+def gitRevisionHash():
+    global revHash
+    revHash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
 
 
 def gitIt():
     rmrfOldData() # Check for existing data / rm rf if exists
     try:
         os.system(CLONE_REPO) # Clone Repo
+        # not sure if this checks for the 128 failed exit code git returns on a bad download, inquire in class
         print("Repo Cloned Successfully!")
     except Exception as ex:
-        exceptionDef(ex)
+        exceptionKillDef(ex)
 
 
 def rmrfOldData():
@@ -38,8 +58,18 @@ def rmrfOldData():
         os.system(RM_RF + REPO_DIR) # Recusively delete files and folders if it does   
 
 
-def exceptionDef(ex):
-    print(ex)        
+def exceptionKillDef(ex):
+    print(ex)
+    try:
+        with open('results.txt', 'w+') as results: 
+            results.write("----------FAILURE----------")
+            results.write(version if version else 'Version: Unavailable')
+            results.write(revHash if revHash else 'Hash: Unavailable')
+            results.write("----------FAILURE----------")
+    except:
+        pass
+
+    sys.exit(1)        
 
 
 if __name__ == "__main__":
