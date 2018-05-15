1e9383e5480c895024e358d0e0c0d5171b85b20e
Chris Peragine <cperagine@gmail.com>
2018-05-14 20:39:38 -0400

    Stop Point

--- automation.py
+++ automation.py
@@ -10,11 +10,20 @@ CLONE_REPO = "git clone https://github.com/ccoble-southhills/cp332-project6.git"
 REPO_DIR = "cp332-project6"
 RM_RF = "rm -rf "
 START_TIME = time.ctime()
+PIC_LINK = 'https://s3.amazonaws.com/southhills/pics/'
 
 #Global Vars
-testCaseResults = {'Pass': 0, 'Fail': 0}
+testCaseResults = {'Attempts': 0, 'Pass': 0, 'Fail': 0}
 version = None
 revHash = None
+# commands Dictonary holds {key=scriptArg, [ExpStatusCode, runtime, md5sum, ]}
+commands = {'dog': [0, 'pic1'], 
+            'ducks': [0, 'pic2'],
+            'flower': [0, 'pic3'],
+            'moon': [0, 'pic4'], 
+            'mountain': [0, 'pic5'],
+            ' ': [2],
+            'sun': [2]}
 
 
 def main():
@@ -23,19 +32,38 @@ def main():
     # Retrieve Repo Hash and Versions
     gitVersion()
     gitRevisionHash()
+    testController()
+
 
     print(START_TIME)
     print(version)
     print(revHash)
 
 
+def testController():
+    global commands
+    os.chdir(REPO_DIR)
+    for key, value in commands.items():
+        runScript(key, value)
+
+
+def runScript(key, value):
+
+    arg = key
+    try:
+        print(os.getcwd())
+        output = subprocess.check_output(['python', 'getpicture', arg], stderr=subprocess.STDOUT).decode('utf-8')
+        print(output)
+    except Exception as ex:
+        print(key + " failed: ", ex)
+
 def gitVersion():
     global version
 
     try:
-        # Change to correct DIR
-        os.chdir(REPO_DIR)
-        with open('VERSION', 'r') as versionFile:
+        # Create var for opening the VERSION file in repo using os
+        verFile = os.path.join(os.getcwd(), REPO_DIR, 'VERSION')
+        with open(verFile, 'r') as versionFile:
             # Grab version, remove newline
             version = versionFile.read()[:-1]
     except Exception as ex:
@@ -44,13 +72,16 @@ def gitVersion():
 
 def gitRevisionHash():
     global revHash
-    revHash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
+    # Grab long version of HEAD from repo to aquire the hash, remove /n and decode b object
+    revHash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip().decode('utf-8') 
 
 
 def gitIt():
-    rmrfOldData() # Check for existing data / rm rf if exists
+    # Check for existing data / rm rf if exists
+    rmrfOldData()
     try:
-        os.system(CLONE_REPO) # Clone Repo
+        # Clone Repo
+        os.system(CLONE_REPO) 
         # not sure if this checks for the 128 failed exit code git returns on a bad download, inquire in class
         print("Repo Cloned Successfully!")
     except Exception as ex:
@@ -58,23 +89,20 @@ def gitIt():
 
 
 def rmrfOldData():
-     if os.path.isdir(REPO_DIR): # Check if directory exists
-        os.system(RM_RF + REPO_DIR) # Recusively delete files and folders if it does   
+    # Check if directory exists
+     if os.path.isdir(REPO_DIR): 
+        # Recusively delete files and folders if it does
+        os.system(RM_RF + REPO_DIR)   
 
 
 def exceptionKillDef(ex):
     print(ex)
-    try:
-        with open('results.txt', 'w+') as results:
-            results.write('------ START FAILURE-------')
-            results.write(START_TIME)
-            results.write(version if version else 'Version: Unavailable')
-            results.write(revHash if revHash else 'Hash: Unavailable')
-            results.write(ex)
-            results.write('^^^^^^^ END FAILURE ^^^^^^^')
-    except:
-        pass
-
+    with open('results.txt', 'a+') as results:
+        results.write('\n------ START FAILURE-------\n')
+        results.write(START_TIME + '\n')
+        results.write(('Version: ' + version + '\n') if version else 'Version: Unavailable\n')
+        results.write(('Hash: ' + revHash + '\n') if revHash else 'Hash: Unavailable\n')
+        results.write('^^^^^^^ END FAILURE ^^^^^^^\n')
     sys.exit(1)        
 
 
